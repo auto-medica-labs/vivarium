5aa59b7b058ec3de0ce0c71501232fdf36223d8a
Dheerapat Tookkane <dheerapat38@gmail.com>
2025-12-11 23:24:54 +0700

    nit: test sse

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,34 @@
-import { Elysia } from "elysia";
+import { Elysia, t, sse } from "elysia";
+import { setTimeout } from "timers/promises";
+import { PythonEnvironment } from "./service/types";
+import { PyodidePythonEnvironment } from "./service/python-interpreter";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+// const pythonEnvironment: PythonEnvironment = new PyodidePythonEnvironment();
+// pythonEnvironment.init();
+
+const app = new Elysia();
+
+app.get("/sse", async function* () {
+    yield sse("hello");
+    await setTimeout(1000);
+    yield sse("world");
+});
+
+app.post(
+    "/exec",
+    async function* ({ body }) {
+        const { code } = body;
+        yield sse(code);
+    },
+    {
+        body: t.Object({
+            code: t.String(),
+        }),
+    },
+);
+
+app.listen(3080);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+    `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
